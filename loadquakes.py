ed36b8a6892a2f4514cc5c947d17629ba9227b18
Amanda Syamsul <amand4@sermeq.ess.washington.edu>
2022-04-12 01:56:39 -0400

    edited freedman-diaconis binsize function

--- loadquakes.py
+++ loadquakes.py
@@ -178,6 +178,35 @@ def get_cond_probability(all_time_periods, earthquake_only, loads):
         
     return np.array(cp_for_each_event)
 
+def freedman_diaconis(data, returnas="width"):
+    """
+    Use Freedman Diaconis rule to compute optimal histogram bin width. 
+    ``returnas`` can be one of "width" or "bins", indicating whether
+    the bin width or number of bins should be returned respectively. 
+
+
+    Parameters
+    ----------
+    data: np.ndarray
+        One-dimensional array.
+
+    returnas: {"width", "bins"}
+        If "width", return the estimated width for each histogram bin. 
+        If "bins", return the number of bins suggested by rule.
+    """
+    data = np.asarray(data, dtype=np.float_)
+    IQR  = stats.iqr(data, rng=(25, 75), scale="raw", nan_policy="omit")
+    N    = data.size
+    bw   = (2 * IQR) / np.power(N, 1/3)
+
+    if returnas=="width":
+        result = bw
+    else:
+        datmin, datmax = data.min(), data.max()
+        datrng = datmax - datmin
+        result = int((datrng / bw) + 1)
+    return(result)
+
 def calculate_bin_sizes(some_data,method):
     xmin=np.min(some_data)
     xmax=np.max(some_data)
@@ -188,7 +217,7 @@ def calculate_bin_sizes(some_data,method):
         bins = np.linspace(xmin, xmax,
                        int(1 + 3.322*np.log(some_data.size)))
     else:
-        bins = np.linspace(xmin, xmax,108)
+        bins = np.linspace(xmin, xmax,freedman_diaconis(data=some_data, returnas="bins"))
     return bins
 
 def calculate_bayes(earthquake_only,all_time_periods,method):
